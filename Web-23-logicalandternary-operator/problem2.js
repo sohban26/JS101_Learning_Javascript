9789d9ac8c9306d2a9f758332f9616a2e1114182
sohban@26 <sohbankhan26@gmail.com>
2022-10-11 01:32:24 +0530

    logical and ternary operators

---
+++ Web-23-logicalandternary-operator/problem2.js
@@ -0,0 +1,7 @@
+// Given any character, if it is a vowel print "Vowel"
+let char="a"
+if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+  console.log("vowel")
+}else{
+  console.log("consonent")
+}
---
+++ Web-23-logicalandternary-operator/problem4.js
@@ -0,0 +1,11 @@
+// Given 3 numbers (all different values), print which is greatest
+let a= 2;
+let b=6;
+let c=1;
+if(a>b&&a>c){
+  console.log(a,"is greater")
+} else if(b>a&&b>c){
+  console.log(b,"is greater")
+} else {
+  console.log(c,"is greater")
+}
